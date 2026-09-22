d573fb1c065cf610b0faa5da1ff4056ca88871a2
Tepah <67202848+Tepah@users.noreply.github.com>
2026-09-21 21:51:56 -0700

    Created a little game design for the box

--- app/src/main/java/com/example/nuzetch/MainActivity.kt
+++ app/src/main/java/com/example/nuzetch/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.nuzetch
 
 import android.os.Bundle
+import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,38 +14,51 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.innerShadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.shadow.Shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.example.nuzetch.model.PokemonType
 import com.example.nuzetch.model.TypeChart
@@ -57,11 +71,86 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NuzetchTheme {
-                    WeaknessChart()
+                    NuzetchDevice()
             }
         }
     }
 }
+
+@Composable
+fun NuzetchDevice(modifier: Modifier = Modifier) {
+    var onApp by remember { mutableIntStateOf(0) }
+    val appCount = 4
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFF646464)
+    ) {
+        Column( modifier = Modifier.fillMaxSize()) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Button(
+                    modifier = Modifier
+                        .height(192.dp)
+                        .width(384.dp),
+                    onClick = ({})
+                )
+                {}
+
+                Row(
+                    modifier = Modifier
+                        .padding(top = 64.dp),
+                    horizontalArrangement = Arrangement.spacedBy(32.dp)
+                )
+                {
+                    for (i in 0 until appCount) {
+                        Box(
+                            modifier = Modifier
+                                .size(16.dp)
+                                .clip(CircleShape)
+                                .background(Color.Gray)
+                                .padding(6.dp),
+                            contentAlignment = Alignment.Center
+                        )
+                        {}
+                    }
+                }
+
+                Button(
+                    modifier = Modifier
+                        .height(192.dp)
+                        .width(384.dp),
+                    onClick = ({})
+                )
+                {}
+            }
+
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .background(Color.White, RoundedCornerShape(32.dp))
+                    .fillMaxSize()
+                    .innerShadow(
+                        shape = RoundedCornerShape(32.dp),
+                        shadow = Shadow(
+                            radius = 8.dp,
+                            spread = 2.dp,
+                            color = Color(0x66000000),
+                            offset = DpOffset(8.dp, 6.dp)
+                        )
+                    )
+            )
+            {
+
+            }
+        }
+    }
+}
+
 
 @Composable
 fun WeaknessChart(modifier: Modifier = Modifier) {
@@ -364,7 +453,15 @@ private fun AnimatedTypeIcon(type: PokemonType, iconSize: Dp) {
 @Preview(showBackground = true, widthDp = 1240, heightDp = 1080, name = "Thor Bottom")
 @Preview(showBackground = true, widthDp = 360, heightDp = 800, name = "Phone")
 @Composable
-fun GreetingPreview() {
+fun NuzetchPreview() {
+    NuzetchTheme {
+        NuzetchDevice()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 1240, heightDp = 1080, name = "Thor Bottom")
+@Composable
+fun WeaknessPreview() {
     NuzetchTheme {
         WeaknessChart()
     }
